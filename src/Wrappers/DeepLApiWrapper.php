80bb9c8accd3b2cbd45dd736b51d374c2892eed6
Joris van Willigen <me@jorisvanw.me>
2019-03-15 17:52:07 +0100

    Prevent translating the :keys (and make it cheaper)

--- src/Wrappers/DeepLApiWrapper.php
+++ src/Wrappers/DeepLApiWrapper.php
@@ -73,10 +73,62 @@ class DeepLApiWrapper
     }
 
     /**
-     * @return \JorisvanW\DeepL\Api\Endpoints\TranslateEndpoint
+     * Translate a collection of translations with \JorisvanW\DeepL\Api\Resources\Translate items from DeepL.
+     *
+     * @param string $text
+     * @param string $to
+     * @param string $from
+     * @param array  $options
+     *
+     * @return \JorisvanW\DeepL\Api\Resources\BaseResource|\JorisvanW\DeepL\Api\Resources\Translate
+     * @throws \JorisvanW\DeepL\Api\Exceptions\ApiException
      */
-    public function translate()
-    {
-        return $this->client->translate;
+    public function translate(
+        $text,
+        $to = TranslateType::LANG_EN,
+        $from = TranslateType::LANG_AUTO,
+        $options = []
+    ) {
+        $regexTemp = [];
+
+        // Prevent translating the :keys (and make it cheaper)
+        if (!array_get($options, 'translate_keys', false) && preg_match_all('~(:\w+)~', $text, $matches, PREG_PATTERN_ORDER)) {
+            foreach ($matches[1] as $key => $word) {
+                $regexTemp["_{$key}"] = $word;
+            }
+
+            $text = str_replace(array_values($regexTemp), array_keys($regexTemp), $text);
+        }
+
+        $response = $this->client->translations->translate($text, $to, $from, $options = []);
+
+        if (!empty($regexTemp)) {
+            foreach ($response->translations as $key => $translation) {
+                /** @var  text */
+                $response->translations[$key]->text = str_replace(array_keys($regexTemp), array_values($regexTemp), $translation->text);
+            }
+        }
+
+        return $response;
+    }
+
+    /**
+     * Translate a text with DeepL.
+     *
+     * @param string $text
+     * @param string $to
+     * @param string $from
+     * @param array  $options
+     *
+     * @return string
+     * @throws \JorisvanW\DeepL\Api\Exceptions\ApiException
+     */
+    public function translateText(
+        $text,
+        $to = TranslateType::LANG_EN,
+        $from = TranslateType::LANG_AUTO,
+        $options = []
+    ) {
+        return $this->translate($text, $to, $from, $options, true)->translations[0]->text;
     }
 }
