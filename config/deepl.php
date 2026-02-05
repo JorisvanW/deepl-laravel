d632f0164c31c3ee796e9d8f973d75bd391c3982
Joris van Willigen <me@jorisvanw.me>
2026-02-05 09:31:11 +0100

    Add option to prevent words to be translated

--- config/deepl.php
+++ config/deepl.php
@@ -4,6 +4,12 @@ return [
 
     'key' => env('DEEPL_KEY'),
 
+    // Should be true in most cases, as it prevents :name and @name to be translated (Like to :naam @naam).
+    'translate_keys' => true,
+
+    // A list of words you do not want to be translared.
+    // For example you business name.
+    'prevent_translation_words' => [],
 
     // DeepL sometimes adds spaces around chars (mostly when it is more then one sentence)
     // Leaving empty array wil not trim
